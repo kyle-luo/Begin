4d7c19f650566df4bf205e683ee985e8f7328c14
kyle-luo <kyle-luo@users.noreply.github.com>
2018-10-28 01:28:09 -0400

    add to index done

---
+++ Python/just begin/add to index.py
@@ -0,0 +1,44 @@
+# Define a procedure, add_to_index,
+# that takes 3 inputs:
+
+# - an index: [[<keyword>,[<url>,...]],...]
+# - a keyword: String
+# - a url: String
+
+# If the keyword is already
+# in the index, add the url
+# to the list of urls associated
+# with that keyword.
+
+# If the keyword is not in the index,
+# add an entry to the index: [keyword,[url]]
+
+index = []
+
+def add_to_index(index,keyword,url):
+    if any(keyword in sublists for sublists in index):
+        for sublists in index:
+            if keyword in sublists:
+                if url not in sublists[1]:
+                    sublists[1].append(url)
+    else:
+        sublist = []
+        sublist.append(keyword)
+        urllist = []
+        urllist.append(url)
+        sublist.append(urllist)
+        index.append(sublist)
+    return index
+
+
+
+
+
+add_to_index(index,'udacity','http://udacity.com')
+add_to_index(index,'computing','http://acm.org')
+add_to_index(index,'udacity','http://npr.org')
+print index
+#>>> [['udacity', ['http://udacity.com', 'http://npr.org']],
+#>>> ['computing', ['http://acm.org']]]
+
+
