6c25856939165850d35aa9756b0a9701ae3fcdec
kyle-luo <tsjlky@gmail.com>
2019-05-18 22:30:48 -0400

    1

---
+++ Algorithm/Save/Selection Sort.py
@@ -0,0 +1,22 @@
+import random
+
+def merge_sort(nums):
+    for i in range(len(nums)):
+        min = i
+        for j in range(i + 1, len(nums)):
+            if nums[j] < nums[min]:
+                min = j
+        nums[i], nums[min] = nums[min], nums[i]
+
+a = [1,564,7,9,79,7,4567,2,31,7,977,98,]
+
+merge_sort(a)
+
+print(a)
+
+b = []
+for x in range(10000):
+    b.append(random.randint(1, 100000))
+
+merge_sort(b)
+print(b)
