827700cd453d0603f7a7f373194ca397d4a85050
kyle-luo <tsjlky@gmail.com>
2019-05-21 23:44:25 -0400

    Update Quick Sort Plus.py

--- Algorithm/Save/Quick Sort Plus.py
+++ Algorithm/Save/Quick Sort Plus.py
@@ -6,87 +6,55 @@ class QuickSortPlus:
         self._sort(A, 0, len(A) - 1)
 
     def _sort(self, A, l, r):
+        mid = int((r - l) / 2)
         if l > r:
             return
+        if A[l] <= A[mid] <= A[r] or A[r] <= A[mid] <= A[l]:
+            k = mid
+        elif A[mid] <= A[l] <= A[r] or A[r] <= A[l] <= A[mid]:
+            k = l
+        else:
+            k = r
+        A[l], A[k] = A[k], A[l]
         m = self._partition(A, l, r)
         self._sort(A, l, m - 1)
         self._sort(A, m + 1, r)
 
     def _partition(self, A, l, r):
-        mid = int((r - l) / 2)
-        if A[l] <= A[mid] <= A[r] or A[r] <= A[mid] <= A[l]:
-            j = mid
-        elif A[mid] <= A[l] <= A[r] or A[r] <= A[l] <= A[mid]:
-            j = l
-        else:
-            j = r
-        pivot = A[j]
-        small = None
-        big = None
-        while l != j and r != j:
-            while big is None and l != j:
-                if A[l] > pivot:
-                    big = l
-                l += 1
-            while small is None and r != j:
-                if A[r] < pivot:
-                    small = r
-                r -= 1
-            try:
-                A[big], A[small] = A[small], A[big]
-            except:
-                break
-            small = None
-            big = None
-        if big is None:
-            if r > j:
-                start = j
-                for i in range(start + 1, r + 1):
-                    if A[i] <= pivot:
-                        j += 1
-                        A[i], A[j] = A[j], A[i]
-                        print(A)
-                A[start], A[j] = A[j], A[start]
-        else:
-            if big > j:
-                start = j
-                for i in range(start + 1, r + 1):
-                    if A[i] <= pivot:
-                        j += 1
-                        A[i], A[j] = A[j], A[i]
-                        print(A)
-                A[start], A[j] = A[j], A[start]
-        if small is None:
-            if l < j:
-                end = j
-                for i in reversed(range(l, end)):
-                    if A[i] >= pivot:
-                        j -= 1
-                        A[i], A[j] = A[j], A[i]
-                        print(A)
-                A[end], A[j] = A[j], A[end]
-        else:
-            if small < j:
-                end = j
-                for i in reversed(range(l, end)):
-                    if A[i] >= pivot:
-                        j -= 1
-                        A[i], A[j] = A[j], A[i]
-                        print(A)
-                A[end], A[j] = A[j], A[end]
+        pivot = A[l]
+        j = l
+        for i in range(l + 1, r + 1):
+            if A[i] <= pivot:
+                j += 1
+                A[i], A[j] = A[j], A[i]
+        A[l], A[j] = A[j], A[l]
         return j
 
 
 quick = QuickSortPlus()
 
+while True:
+    a = [random.randint(1, 1000) for x in range(10)]
+    b = a
+    c = a
+    quick.sort(b)
+    c.sort()
+    if b == c:
+        print("ok")
+    else:
+        print("failed")
+        print(b)
+        print(c)
+        break
+
 # a = [100,564,7,9,79,99,4567,2,31,7,977,98]
 # quick.sort(a)
 # print(a)
 
-a = [random.randint(1, 100) for x in range(10)]
-print(a)
-quick.sort(a)
-print(a)
+# a = [random.randint(1, 100) for x in range(10)]
+# print(a)
+# quick.sort(a)
+# print(a)
 
 # b = [random.randint(1, 10000) for x in range(1000000)]
 # start = time.time()
