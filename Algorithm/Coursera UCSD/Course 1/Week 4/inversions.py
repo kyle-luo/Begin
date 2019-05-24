8a636e64253193798b8051a17b4b1611ec6e5056
kyle-luo <tsjlky@gmail.com>
2019-05-23 20:06:59 -0400

    1

--- Algorithm/Coursera UCSD/Course 1/Week 4/inversions.py
+++ Algorithm/Coursera UCSD/Course 1/Week 4/inversions.py
@@ -1,34 +1,46 @@
 # Uses python3
 import sys
 
+
 def get_number_of_inversions(a, b, left, right):
     number_of_inversions = 0
-    if right - left <= 1:
+    if right <= left:
         return number_of_inversions
-    ave = (left + right) // 2
+    ave = left + (right - left) // 2
     number_of_inversions += get_number_of_inversions(a, b, left, ave)
     number_of_inversions += get_number_of_inversions(a, b, ave + 1, right)
-    number_of_inversions += merge(a, b, )
-    #write your code here
-
+    number_of_inversions += merge(a, b, ave, left, right)
     return number_of_inversions
-    # count = 0
-    # if len(a) == 1:
-    #     return a
-    # m = int(len(a) / 2)
-    # b = get_number_of_inversions(a[:m])[0]
-    # c = get_number_of_inversions(a[m:])[0]
-    # return merge(b, c, count)
 
-def merge(a, b, left, right):
+
+def merge(a, b, m, l, r):
     count = 0
-    l, m, r =
+    i = l
+    j = m + 1
+    cur = l
+    while i <= m and j <= r:
+        if a[i] < a[j]:
+            b[cur] = a[i]
+            i += 1
+        else:
+            b[cur] = a[j]
+            j += 1
+            count += 1
+        cur += 1
+    while i <= m:
+        b[cur] = a[i]
+        i += 1
+        cur += 1
+    while j <= r:
+        b[cur] = a[j]
+        j += 1
+        cur += 1
+    return count
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     b = n * [0]
-    # print(get_number_of_inversions(a, b, 0, len(a)))
-    print(get_number_of_inversions(a)[1])
+    print(get_number_of_inversions(a, b, 0, len(a) - 1))
 
