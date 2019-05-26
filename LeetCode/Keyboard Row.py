e087fcc22e4173fd1f5e111cfab677ab69471832
kyle-luo <tsjlky@gmail.com>
2019-05-26 00:48:42 -0400

    Update Keyboard Row.py

--- LeetCode/Keyboard Row.py
+++ LeetCode/Keyboard Row.py
@@ -1,3 +1,5 @@
+import re
+
 # Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
 #
 # Example:
@@ -33,4 +35,33 @@ class Solution:
                     cur += 1
             if check == True:
                 output.append(word)
+        return output
+
+
+class Solution2:
+    def findWords(self, words):
+        rows = re.compile('(?i)([qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*)$')
+        return list(filter(rows.match, words))
+
+
+class Solution3:
+    def findWords(self, words):
+        self.rows = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+        self.output = []
+        return list(filter(self.check, words))
+
+    def check(self, word):
+        output = []
+        row = 0
+        while row < len(self.rows):
+            if word[0].lower() in self.rows[row]:
+                cur = 1
+                check = True
+                while check is True and cur < len(word):
+                    if word[cur].lower() not in self.rows[row]:
+                        check = False
+                    cur += 1
+                if check == True:
+                    output.append(word)
+            row += 1
         return output
