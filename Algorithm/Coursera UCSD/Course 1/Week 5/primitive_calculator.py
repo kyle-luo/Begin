23f2b599c9bfa05d7ed2bcc8cb504a45ecfc6b2a
kyle-luo <tsjlky@gmail.com>
2019-06-01 01:36:56 -0400

    Update primitive_calculator.py

--- Algorithm/Coursera UCSD/Course 1/Week 5/primitive_calculator.py
+++ Algorithm/Coursera UCSD/Course 1/Week 5/primitive_calculator.py
@@ -13,9 +13,33 @@ def optimal_sequence(n):
             n = n - 1
     return reversed(sequence)
 
+def optimal_sequence_rec(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    elif n == 3:
+        return 1
+    else:
+        a = b = n
+        if n % 3 == 0:
+            a = optimal_sequence_rec(n // 3) + 1
+        if n % 2 == 0:
+            b = optimal_sequence_rec(n // 2) + 1
+        c = optimal_sequence_rec(n - 1) + 1
+        return min(a, b, c)
+
+def optimal_sequence_dp(n):
+
+
 input = sys.stdin.read()
 n = int(input)
 sequence = list(optimal_sequence(n))
 print(len(sequence) - 1)
+
 for x in sequence:
     print(x, end=' ')
+
+print("\n")
+
+print(optimal_sequence_rec(n))
