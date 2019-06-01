bd2ee8119da8956401f1a561098f7ff8470dfa98
kyle-luo <tsjlky@gmail.com>
2019-06-01 16:32:29 -0400

    Update edit_distance.py

--- Algorithm/Coursera UCSD/Course 1/Week 5/edit_distance.py
+++ Algorithm/Coursera UCSD/Course 1/Week 5/edit_distance.py
@@ -1,11 +1,25 @@
 # Uses python3
 def edit_distance(s, t):
-    s = [x for x in s]
-    t = [x for x in t]
-    for i in len(s):
-        for j in len(t):
-            insertion = 
-    return 0
+    s = [None] + [x for x in s]
+    t = [None] + [x for x in t]
+
+def edit_rec(s, t, i, j):
+    if i == 0 and j == 0:
+        return 0
+    a = b = c = d = max(len(s), len(t))
+    if s[i] == t[j] and i > 0 and j > 0:
+        a = edit_rec(s, t, i - 1, j - 1)
+    if s[i] == t[j - 1] and j > 0:
+        b = edit_rec(s, t, i, j - 1) + 1
+    if s[i - 1] == t[j] and i > 0:
+        c = edit_rec(s, t, i - 1, j) + 1
+    if s[i - 1] != t[j - 1] and i > 0 and j > 0:
+        d = edit_rec(s, t, i - 1, j - 1) + 1
+    return min(a, b, c, d)
 
 if __name__ == "__main__":
-    print(edit_distance(input(), input()))
+    # print(edit_distance(input(), input()))
+    s, t = input(), input()
+    s = [None] + [x for x in s]
+    t = [None] + [x for x in t]
+    print(edit_rec(s, t, len(s) - 1, len(t) - 1))
