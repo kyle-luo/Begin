5405b7bbc4f101511dd20051e7fc8c3fa1586fd7
kyle-luo <tsjlky@gmail.com>
2019-06-02 23:32:24 -0400

    Update lcs3.py

--- Algorithm/Coursera UCSD/Course 1/Week 5/lcs3.py
+++ Algorithm/Coursera UCSD/Course 1/Week 5/lcs3.py
@@ -1,10 +1,23 @@
 #Uses python3
-
 import sys
 
 def lcs3(a, b, c):
-    #write your code here
-    return min(len(a), len(b), len(c))
+    rec = [[[0 for _ in range(len(c) + 1)] for _ in range(len(b) + 1)] for _ in range(len(a) + 1)]
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            for k in range(1, len(c) + 1):
+                if a[i - 1] == b[j - 1] and a[i - 1] == c[k - 1]:
+                    rec[i][j][k] = rec[i - 1][j - 1][k - 1] + 1
+                else:
+                    x = rec[i - 1][j][k]
+                    y = rec[i][j - 1][k]
+                    z = rec[i][j][k - 1]
+                    d = rec[i - 1][j - 1][k]
+                    e = rec[i - 1][j][k - 1]
+                    f = rec[i][j - 1][k - 1]
+                    g = rec[i - 1][j - 1][k - 1]
+                    rec[i][j][k] = max(x, y, z, d, e, f, g)
+    return rec[len(a)][len(b)][len(c)]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -20,4 +33,6 @@ if __name__ == '__main__':
     cn = data[0]
     data = data[1:]
     c = data[:cn]
+    # print(a)
+    # print(type(a))
     print(lcs3(a, b, c))
