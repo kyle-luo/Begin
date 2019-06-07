9733b57bede46a9cb4806e7d4a15e8e3458140af
kyle-luo <tsjlky@gmail.com>
2019-06-07 00:38:02 -0400

    Create Binary Number with Alternating Bits.py

---
+++ LeetCode/Binary Number with Alternating Bits.py
@@ -0,0 +1,38 @@
+# Given a positive integer, check whether it has alternating bits: namely, if two adjacent bits will always have different values.
+#
+# Example 1:
+# Input: 5
+# Output: True
+# Explanation:
+# The binary representation of 5 is: 101
+# Example 2:
+# Input: 7
+# Output: False
+# Explanation:
+# The binary representation of 7 is: 111.
+# Example 3:
+# Input: 11
+# Output: False
+# Explanation:
+# The binary representation of 11 is: 1011.
+# Example 4:
+# Input: 10
+# Output: True
+# Explanation:
+# The binary representation of 10 is: 1010.
+#
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        bi = bin(n)[2:]
+        i = 0
+        while i < len(bi):
+            if bi[i] != '1':
+                return False
+            i += 2
+        j = 1
+        while j < len(bi):
+            if bi[j] != '0':
+                return False
+            j += 2
+        return True
