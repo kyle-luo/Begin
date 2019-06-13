076817a2fff2f34f235c9216b1ecff2a558b46ac
kyle-luo <tsjlky@gmail.com>
2019-06-13 01:30:34 -0400

    1

--- Flask/flask-bootstrap.py
+++ Flask/flask-bootstrap.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bs = Bootstrap(app)
+
 
 @app.route('/')
 
 def go():
     title = 'render template'
     p = ['lao da', 'lao er', 'lao san']
-    # return render_template('render templates.html')
+    return render_template('nav.html')
     # return render_template('render templates.html', title=title)
-    return render_template('render templates.html', title=title, data=p)
+    # return render_template('nav.html', title=title, data=p)
 
 if __name__ == '__main__':
     app.run(debug=True)
