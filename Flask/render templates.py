37f5bd3e99c891168a3d7a12a08c67910a82ecbb
kyle-luo <tsjlky@gmail.com>
2019-06-12 22:39:27 -0400

    1

--- Flask/render templates.py
+++ Flask/render templates.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+@app.route('/')
 
 def hw():
-    return render_template('index.html')
+    title = 'try render template'
+    return render_template('render template.html', title=title)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
+
