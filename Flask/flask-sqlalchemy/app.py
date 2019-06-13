1308f53181db7ce53983dcb200ea8ce18fa6149e
kyle-luo <tsjlky@gmail.com>
2019-06-13 19:50:20 -0400

    Update app.py

--- Flask/flask-sqlalchemy/app.py
+++ Flask/flask-sqlalchemy/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+from config import Config
+
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+app.config.from_object(Config)
+
 @app.route('/')
 
-def go():
-    title = 'render template'
-    p = ['lao da', 'lao er', 'lao san']
-    # return render_template('render templates.html')
-    # return render_template('render templates.html', title=title)
-    return render_template('render templates.html', title=title, data=p)
+# def go():
+#     title = 'render template'
+#     p = ['lao da', 'lao er', 'lao san']
+#     # return render_template('render templates.html')
+#     # return render_template('render templates.html', title=title)
+#     return render_template('render templates.html', title=title, data=p)
+
+def run():
+    return "test"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
