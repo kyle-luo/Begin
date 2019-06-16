57f11a0b68b96aaef59db558039c42f598d925ec
kyle-luo <tsjlky@gmail.com>
2019-06-16 17:10:38 -0400

    1

--- Flask/flask-WTF/forms.py
+++ Flask/flask-WTF/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
-    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo(password)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired(message='Please Enter a Username'), Length(min=6, max=20)])
+    password = PasswordField('Password', validators=[DataRequired(message= u'密码不能为空'), Length(min=6, max=20, message= u'密码不能为空')])
+    confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo(password, message= u'密码不能为空')])
+    email = StringField('Email', validators=[DataRequired(), Email(message= u'请输入有效的邮箱地址，比如：username@domain.com')])
     submit = SubmitField('Register')
