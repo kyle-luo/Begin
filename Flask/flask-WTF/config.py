b1ff93cafae79c1a45d710b050a4b6379901703b
kyle-luo <tsjlky@gmail.com>
2019-06-16 00:29:58 -0400

    1

--- Flask/flask-WTF/config.py
+++ Flask/flask-WTF/config.py
@@ -4,5 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     #Database configuration
-    SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlist:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlist:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlist:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
