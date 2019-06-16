d44bcfb86cc397244f8371571b847430d82584df
kyle-luo <tsjlky@gmail.com>
2019-06-16 01:42:42 -0400

    1

--- Flask/flask-WTF/app.py
+++ Flask/flask-WTF/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
 app = Flask(__name__)
+
+bs = Bootstrap(app)
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -11,10 +14,11 @@ db = SQLAlchemy(app)
 @app.route('/')
 
 def go():
-    return render_template('render templates.html')
+    return render_template('nav.html')
 
-# def register():
-#     return render_template('register.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
