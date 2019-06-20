b0d9e812f9b8ff7df9b461b14103ebee49dee403
kyle-luo <tsjlky@gmail.com>
2019-06-20 01:46:59 -0400

    1

--- Flask/flask-WTF/venv/forms.py
+++ Flask/flask-WTF/venv/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import Form, StringField, PasswordField, SubmitField, validators
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=20)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
     confirm_password = PasswordField('Password', validators=[DataRequired(), EqualTo(password)])
     email = StringField('Email', validators=[DataRequired(), Email()])
