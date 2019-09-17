6858d120799c79f098a0d95b6a08f0030af802a6
kyle-luo <tsjlky@gmail.com>
2019-09-16 20:17:15 -0400

    Update partition3.py

--- Algorithm/Coursera UCSD/Course 1/Week 6/partition3.py
+++ Algorithm/Coursera UCSD/Course 1/Week 6/partition3.py
@@ -2,15 +2,25 @@
 import sys
 import itertools
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+def partition3(values):
+    t = sum(values)
+    if t % 3 != 0 or len(values) < 3:
+        return 0
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
+    pp = t // 3
+    print(pp)
 
+    dp = [0 for _ in range(t + 1)]
+    count = 0
+
+    for i in range(len(values)):
+        for j in reversed(range(pp + 1)):
+            if values[i] <= j:
+                dp[j] = max(dp[j], values[i] + dp[j - values[i]])
+                if dp[j] == pp:
+                    count += 1
+    if count == 3:
+        return 1
     return 0
 
 if __name__ == '__main__':
