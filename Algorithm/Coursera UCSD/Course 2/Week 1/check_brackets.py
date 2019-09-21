79415b7b70cadf62b6de921b61c8d020e317dafc
kyle-luo <tsjlky@gmail.com>
2019-09-20 20:48:29 -0400

    stack

--- Algorithm/Coursera UCSD/Course 2/Week 1/check_brackets.py
+++ Algorithm/Coursera UCSD/Course 2/Week 1/check_brackets.py
@@ -11,14 +11,24 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    opening_brackets_indexs = []
     for i, next in enumerate(text):
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append(next)
+            opening_brackets_indexs.append(i)
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+            last = opening_brackets_stack.pop()
+            if last == "(" and next != ")":
+                return i + 1
+            if last == "[" and next != "]":
+                return i + 1
+            if last == "{" and next != "}":
+                return i + 1
+            opening_brackets_indexs.pop()
+    if len(opening_brackets_stack) == 0:
+        return "Success"
+    else:
+        return opening_brackets_indexs.pop() + 1
 
 
 def main():
