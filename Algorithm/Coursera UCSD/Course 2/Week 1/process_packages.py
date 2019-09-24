694641346f6a12eca6fa44534c99733d3af2f8ab
kyle-luo <tsjlky@gmail.com>
2019-09-23 22:32:12 -0400

    Update process_packages.py

--- Algorithm/Coursera UCSD/Course 2/Week 1/process_packages.py
+++ Algorithm/Coursera UCSD/Course 2/Week 1/process_packages.py
@@ -1,5 +1,6 @@
 # python3
 
+import collections
 from collections import namedtuple
 
 Request = namedtuple("Request", ["arrived_at", "time_to_process"])
@@ -9,14 +10,25 @@ Response = namedtuple("Response", ["was_dropped", "started_at"])
 class Buffer:
     def __init__(self, size):
         self.size = size
-        self.finish_time = []
+        self.finish_time = collections.deque()
 
     def process(self, request):
         # write your code here
-        return Response(False, -1)
+        load = 0
+        for quest in request:
+            if load == 0:
+                load += 1
+                self.finish_time.append(quest[1])
+                return quest[0]
+
+
+
+        # return Response(False, -1)
 
 
 def process_requests(requests, buffer):
+    if buffer == 0 and len(requests) > 0:
+        return -1
     responses = []
     for request in requests:
         responses.append(buffer.process(request))
