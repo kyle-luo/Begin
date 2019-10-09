302ec04d1e1347183ddf8a56e3eca99cc1bb6e1c
kyle-luo <tsjlky@gmail.com>
2019-10-08 21:01:45 -0400

    Update max_sliding_window.py

--- Algorithm/Coursera UCSD/Course 2/Week 1/max_sliding_window.py
+++ Algorithm/Coursera UCSD/Course 2/Week 1/max_sliding_window.py
@@ -1,5 +1,5 @@
 # python3
-
+import collections
 
 def max_sliding_window_naive(sequence, m):
     maximums = []
@@ -8,11 +8,26 @@ def max_sliding_window_naive(sequence, m):
 
     return maximums
 
+def max_sliding_window(sequence, m):
+    track = collections.deque()
+    output = []
+    for i in range(len(sequence)):
+        if track and track[0] < i - m + 1:
+            track.popleft()
+        while track and sequence[track[-1]] < sequence[i]:
+            track.pop()
+        track.append(i)
+        if i >= m - 1:
+            output.append(sequence[track[0]])
+    return output
+
+
+
 if __name__ == '__main__':
     n = int(input())
     input_sequence = [int(i) for i in input().split()]
     assert len(input_sequence) == n
     window_size = int(input())
 
-    print(*max_sliding_window_naive(input_sequence, window_size))
+    print(*max_sliding_window(input_sequence, window_size))
 
